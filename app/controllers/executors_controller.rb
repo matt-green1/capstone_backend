78a6ec648d4455f4a76c80d938bf24d55491fb3b
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-09-18 11:22:45 -0400

    exec form logic, create, edit, delete done

--- app/controllers/executors_controller.rb
+++ app/controllers/executors_controller.rb
@@ -1,6 +1,23 @@
 class ExecutorsController < ApplicationController
-    def index
-        executors = Executor.all
-        render json: executors
+    def create
+        executor = Executor.create(executor_params)
+        render json: executor
     end
+
+    def update
+        executor = Executor.find(params[:id])
+        executor.update(executor_params)
+        render json: executor
+    end
+
+    def destroy
+        executor = Executor.find(params[:id])
+        executor.destroy()
+    end
+
+    private
+    def executor_params
+        params.require(:executor).permit(:user_id, :executor_name, :executor_email, :instructions)
+    end
+
 end
