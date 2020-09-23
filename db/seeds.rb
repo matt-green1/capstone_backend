5d5519b833d75ae3310b7bdd28f7262667e27bb0
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-09-23 14:53:35 -0400

    emails and seeds set to present

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,10 @@ e2 = Executor.create(user:u1, executor_name: "Dee", executor_email: "matthelps33
 
 e3 = Executor.create(user:u2, executor_name: "Julia", executor_email: "matthelps33@gmail.com", relationship: "Sister")
 
-l1 = Letter.create(user: u1, letter_title: "College Friends", recipient_name: "Ethan", recipient_email: "ethan@gmail.com", letter_text: "Hey everyone, so... I did a thing...", signoff: "Matt", letter_instructions: "Use Sam as a point person")
-l2 = Letter.create(user: u1, letter_title: "Julia - thank you", recipient_name: "Julia Pearlman", recipient_email: "julia@gmail.com", letter_text: "Hey Julia, so... I did a thing...", signoff: "Matt", letter_instructions: "N/A")
+l1 = Letter.create(user: u1, letter_title: "College Friends", recipient_name: "Ethan Robinson", recipient_email: "ethan@aol.com", letter_text: "Hey Guys, Just wanted to say that I'm so grateful that I met all of you...", signoff: "I'll miss you all, Matt", letter_instructions: "Tell Ethan to distribute to Sam, Eli, and James")
+l2 = Letter.create(user: u1, letter_title: "Julia thank you", recipient_name: "Julia Pearlman", recipient_email: "julia@aol.com", letter_text: "Hey Julia, I know it's been a while, but I wanted to thank you for...", signoff: "Love, Matt", letter_instructions: "N/A")
 
-l3 = Letter.create(user: u2, letter_title: "Dave - sorry", recipient_name: "Dave Bendet", recipient_email: "dave@gmail.com", letter_text: "Hey Dave, so... I did a thing...", signoff: "Danny", letter_instructions: "If he doesn't response try his cell: 987-473-3903")
+l3 = Letter.create(user: u2, letter_title: "Dave memories", recipient_name: "Dave Bendet", recipient_email: "dave@aol.com", letter_text: "Hey Dave, so... I did a thing...", signoff: "Danny", letter_instructions: "If he doesn't response try his cell: 987-473-3903")
 
 
 
